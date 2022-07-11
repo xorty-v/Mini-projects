dd67df6e5bb5559cb9b40409b1c7e0e5e6077753
Ramaz Gvindzhiliya <gvindzhiliya.ramazi@mail.ru>
2022-07-11 20:38:17 +0400

    Add project - Tic Tac Toe

---
+++ Tic_Tac_Toe/Tic_Tac_Toe/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace Tic_Tac_Toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.but_1 = new System.Windows.Forms.Button();
+            this.but_2 = new System.Windows.Forms.Button();
+            this.but_3 = new System.Windows.Forms.Button();
+            this.but_6 = new System.Windows.Forms.Button();
+            this.but_5 = new System.Windows.Forms.Button();
+            this.but_4 = new System.Windows.Forms.Button();
+            this.but_9 = new System.Windows.Forms.Button();
+            this.but_8 = new System.Windows.Forms.Button();
+            this.but_7 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // but_1
+            // 
+            this.but_1.BackColor = System.Drawing.Color.White;
+            this.but_1.FlatAppearance.BorderSize = 0;
+            this.but_1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.but_1.Font = new System.Drawing.Font("Comic Sans MS", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.but_1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.but_1.Location = new System.Drawing.Point(50, 34);
+            this.but_1.Name = "but_1";
+            this.but_1.Size = new System.Drawing.Size(120, 120);
+            this.but_1.TabIndex = 0;
+            this.but_1.TabStop = false;
+            this.but_1.UseVisualStyleBackColor = false;
+            this.but_1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // but_2
+            // 
+            this.but_2.BackColor = System.Drawing.Color.White;
+            this.but_2.FlatAppearance.BorderSize = 0;
+            this.but_2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.but_2.Font = new System.Drawing.Font("Comic Sans MS", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.but_2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.but_2.Location = new System.Drawing.Point(195, 34);
+            this.but_2.Name = "but_2";
+            this.but_2.Size = new System.Drawing.Size(120, 120);
+            this.but_2.TabIndex = 1;
+            this.but_2.TabStop = false;
+            this.but_2.UseVisualStyleBackColor = false;
+            this.but_2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // but_3
+            // 
+            this.but_3.BackColor = System.Drawing.Color.White;
+            this.but_3.FlatAppearance.BorderSize = 0;
+            this.but_3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.but_3.Font = new System.Drawing.Font("Comic Sans MS", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.but_3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.but_3.Location = new System.Drawing.Point(340, 34);
+            this.but_3.Name = "but_3";
+            this.but_3.Size = new System.Drawing.Size(120, 120);
+            this.but_3.TabIndex = 2;
+            this.but_3.TabStop = false;
+            this.but_3.UseVisualStyleBackColor = false;
+            this.but_3.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // but_6
+            // 
+            this.but_6.BackColor = System.Drawing.Color.White;
+            this.but_6.FlatAppearance.BorderSize = 0;
+            this.but_6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.but_6.Font = new System.Drawing.Font("Comic Sans MS", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.but_6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.but_6.Location = new System.Drawing.Point(340, 178);
+            this.but_6.Name = "but_6";
+            this.but_6.Size = new System.Drawing.Size(120, 120);
+            this.but_6.TabIndex = 5;
+            this.but_6.TabStop = false;
+            this.but_6.UseVisualStyleBackColor = false;
+            this.but_6.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // but_5
+            // 
+            this.but_5.BackColor = System.Drawing.Color.White;
+            this.but_5.FlatAppearance.BorderSize = 0;
+            this.but_5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.but_5.Font = new System.Drawing.Font("Comic Sans MS", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.but_5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.but_5.Location = new System.Drawing.Point(195, 178);
+            this.but_5.Name = "but_5";
+            this.but_5.Size = new System.Drawing.Size(120, 120);
+            this.but_5.TabIndex = 4;
+            this.but_5.TabStop = false;
+            this.but_5.UseVisualStyleBackColor = false;
+            this.but_5.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // but_4
+            // 
+            this.but_4.BackColor = System.Drawing.Color.White;
+            this.but_4.FlatAppearance.BorderSize = 0;
+            this.but_4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.but_4.Font = new System.Drawing.Font("Comic Sans MS", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.but_4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.but_4.Location = new System.Drawing.Point(50, 179);
+            this.but_4.Name = "but_4";
+            this.but_4.Size = new System.Drawing.Size(120, 120);
+            this.but_4.TabIndex = 3;
+            this.but_4.TabStop = false;
+            this.but_4.UseVisualStyleBackColor = false;
+            this.but_4.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // but_9
+            // 
+            this.but_9.BackColor = System.Drawing.Color.White;
+            this.but_9.FlatAppearance.BorderSize = 0;
+            this.but_9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.but_9.Font = new System.Drawing.Font("Comic Sans MS", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.but_9.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.but_9.Location = new System.Drawing.Point(340, 324);
+            this.but_9.Name = "but_9";
+            this.but_9.Size = new System.Drawing.Size(120, 120);
+            this.but_9.TabIndex = 8;
+            this.but_9.TabStop = false;
+            this.but_9.UseVisualStyleBackColor = false;
+            this.but_9.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // but_8
+            // 
+            this.but_8.BackColor = System.Drawing.Color.White;
+            this.but_8.FlatAppearance.BorderSize = 0;
+            this.but_8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.but_8.Font = new System.Drawing.Font("Comic Sans MS", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.but_8.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.but_8.Location = new System.Drawing.Point(195, 324);
+            this.but_8.Name = "but_8";
+            this.but_8.Size = new System.Drawing.Size(120, 120);
+            this.but_8.TabIndex = 7;
+            this.but_8.TabStop = false;
+            this.but_8.UseVisualStyleBackColor = false;
+            this.but_8.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // but_7
+            // 
+            this.but_7.BackColor = System.Drawing.Color.White;
+            this.but_7.FlatAppearance.BorderSize = 0;
+            this.but_7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.but_7.Font = new System.Drawing.Font("Comic Sans MS", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.but_7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.but_7.Location = new System.Drawing.Point(50, 324);
+            this.but_7.Name = "but_7";
+            this.but_7.Size = new System.Drawing.Size(120, 120);
+            this.but_7.TabIndex = 6;
+            this.but_7.TabStop = false;
+            this.but_7.UseVisualStyleBackColor = false;
+            this.but_7.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(38, 305);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(434, 13);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Black;
+            this.pictureBox3.Location = new System.Drawing.Point(38, 160);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(434, 13);
+            this.pictureBox3.TabIndex = 13;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Black;
+            this.pictureBox4.Location = new System.Drawing.Point(176, 23);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(13, 434);
+            this.pictureBox4.TabIndex = 14;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.Black;
+            this.pictureBox5.Location = new System.Drawing.Point(321, 23);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(13, 434);
+            this.pictureBox5.TabIndex = 15;
+            this.pictureBox5.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(506, 478);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.but_9);
+            this.Controls.Add(this.but_8);
+            this.Controls.Add(this.but_7);
+            this.Controls.Add(this.but_6);
+            this.Controls.Add(this.but_5);
+            this.Controls.Add(this.but_4);
+            this.Controls.Add(this.but_3);
+            this.Controls.Add(this.but_2);
+            this.Controls.Add(this.but_1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Крестики-Нолики";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button but_1;
+        private System.Windows.Forms.Button but_2;
+        private System.Windows.Forms.Button but_3;
+        private System.Windows.Forms.Button but_6;
+        private System.Windows.Forms.Button but_5;
+        private System.Windows.Forms.Button but_4;
+        private System.Windows.Forms.Button but_9;
+        private System.Windows.Forms.Button but_8;
+        private System.Windows.Forms.Button but_7;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+    }
+}
+
---
+++ Tic_Tac_Toe/Tic_Tac_Toe/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int count = 0;
+
+        private void DetermineTheMove(object senderObj)
+        {
+            string buttonText = ((Button)senderObj).Text;
+
+            if (buttonText == "")
+            {
+                if (count % 2 == 0)
+                {
+                    ((Button)senderObj).Text = "你";
+                    ((Button)senderObj).ForeColor = Color.Black;
+
+                }
+                else
+                {
+                    ((Button)senderObj).Text = "好";
+                    ((Button)senderObj).ForeColor = Color.DarkBlue;
+                }
+
+                count++;
+            }
+            else
+            {
+                MessageBox.Show("Invalid Click");
+            }
+
+            CheckWin(senderObj);
+        }
+
+        private void CheckWin(object senderObj)
+        {
+            if (but_1.Text == but_2.Text && but_2.Text == but_3.Text && but_1.Text != "") { MessageBox.Show($"Win - {((Button)senderObj).Text}"); RestartGame(); }
+            if (but_4.Text == but_5.Text && but_5.Text == but_6.Text && but_4.Text != "") { MessageBox.Show($"Win - {((Button)senderObj).Text}"); RestartGame(); }
+            if (but_7.Text == but_8.Text && but_8.Text == but_9.Text && but_7.Text != "") { MessageBox.Show($"Win - {((Button)senderObj).Text}"); RestartGame(); }
+            if (but_1.Text == but_4.Text && but_4.Text == but_7.Text && but_1.Text != "") { MessageBox.Show($"Win - {((Button)senderObj).Text}"); RestartGame(); }
+            if (but_2.Text == but_5.Text && but_5.Text == but_8.Text && but_2.Text != "") { MessageBox.Show($"Win - {((Button)senderObj).Text}"); RestartGame(); }
+            if (but_3.Text == but_6.Text && but_6.Text == but_9.Text && but_3.Text != "") { MessageBox.Show($"Win - {((Button)senderObj).Text}"); RestartGame(); }
+            if (but_1.Text == but_5.Text && but_5.Text == but_9.Text && but_1.Text != "") { MessageBox.Show($"Win - {((Button)senderObj).Text}"); RestartGame(); }
+            if (but_3.Text == but_5.Text && but_5.Text == but_7.Text && but_3.Text != "") { MessageBox.Show($"Win - {((Button)senderObj).Text}"); RestartGame(); }
+        }
+
+        private void RestartGame()
+        {
+            count = 0;
+            but_1.Text = "";
+            but_2.Text = "";
+            but_3.Text = "";
+            but_6.Text = "";
+            but_5.Text = "";
+            but_4.Text = "";
+            but_9.Text = "";
+            but_8.Text = "";
+            but_7.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DetermineTheMove(sender);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            RestartGame();
+        }
+    }
+}
